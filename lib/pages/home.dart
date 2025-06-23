d9f290042f8b7f7d3c1fe5906513f56c71befe3f
Bohdan <protzzz.it@gmail.com>
2025-06-23 23:18:53 +0200

    update: improvement of using the certain object for state-changing, not an entire screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,35 +18,35 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final counter = counterCubit.state;
 
-    return BlocBuilder<CounterCubit, int>(
-      bloc: counterCubit,
-      builder: (context, counter) {
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            title: Text(widget.title),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text('You have pushed the button this many times:'),
-                Text(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            BlocBuilder<CounterCubit, int>(
+              bloc: counterCubit,
+              builder: (context, counter) {
+                return Text(
                   '$counter',
                   style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
+                );
+              },
             ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              counterCubit.increment();
-            },
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-        );
-      },
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counterCubit.increment();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
