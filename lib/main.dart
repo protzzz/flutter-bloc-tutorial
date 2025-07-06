be0b8e437264af7c6c0b81632a24c52ae6397f05
Bohdan <protzzz.it@gmail.com>
2025-07-06 03:24:08 +0200

    added CounterBloc to CounterCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:counter/bloc/counter_bloc.dart';
+import 'package:counter/cubit/counter_cubit.dart';
 import 'package:counter/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +13,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: BlocProvider(
+        create: (_) => CounterBloc(),
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          ),
+          home: const HomePage(title: 'Flutter Demo Home Page'),
+        ),
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
