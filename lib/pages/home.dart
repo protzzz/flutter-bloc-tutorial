85e3623bf529918f7c45beb12f6a95eb8a3bb269
Bohdan <protzzz.it@gmail.com>
2025-07-06 03:20:39 +0200

    adapted HomePage to the new IncDecPage with all the funcs

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,27 +1,21 @@
 import 'package:counter/cubit/counter_cubit.dart';
+import 'package:counter/pages/inc_dec.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final counterCubit = CounterCubit();
-
-  @override
   Widget build(BuildContext context) {
-    final counter = counterCubit.state;
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -41,11 +35,13 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        heroTag: 'home',
         onPressed: () {
-          counterCubit.increment();
+          Navigator.of(
+            context,
+          ).push(MaterialPageRoute(builder: (context) => IncDecPage()));
         },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Icon(Icons.navigate_next),
       ),
     );
   }
